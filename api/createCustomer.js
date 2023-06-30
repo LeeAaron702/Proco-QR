a45f192d34a711516048c5fdbf021d76940fefca
Lee Seaver <lee.seaver@gmail.com>
2023-06-30 16:06:09 -0700

    country added

--- api/createCustomer.js
+++ api/createCustomer.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
               city,
               province: state,
               zip: zipcode,
+              country: "US"
             },
           ],
         },
