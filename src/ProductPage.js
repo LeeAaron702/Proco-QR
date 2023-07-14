05bfa51097881e2e67be7c7200a3de46989cd67f
Lee Seaver <lee.seaver@gmail.com>
2023-07-14 14:15:20 -0700

    No data updated landing page.

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -125,8 +125,6 @@ function ProductPage({ data }) {
   }
 
   return (
-
-
     <div className="container">
       <div className="card mt-3">
         <div className="card-body">
@@ -148,7 +146,9 @@ function ProductPage({ data }) {
         </div>
         <div className="button-container text-center">
           <div className="mb-3">
+            {data.amazonLink && 
             <a href={data.shopifyLink} target="_blank" rel="noreferrer" className="btn btn-success btn-lg">Purchase Directly From Us and Save</a>
+            }
           </div>
           <div className="mb-3">
             {data.amazonLink &&
