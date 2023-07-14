5621ad07f272c00669359e4ed7629b096dc85de2
Lee Seaver <lee.seaver@gmail.com>
2023-07-14 14:06:58 -0700

    slack error notification system

--- api/InstantReplacement.js
+++ api/InstantReplacement.js
@@ -13,13 +13,14 @@ export default async function handler(req, res) {
     return res.status(200).json({ message: 'Customer and order created successfully.', customer, order });
   } catch (error) {
     console.error(error);
+    await notifyErrorToSlack(error.message);
     return res.status(500).json({ message: 'Server error.' });
   }
 }
 
 function extractCustomerData(body) {
   const { firstName, lastName, phone, email, address1, address2, city, state, zipcode } = body;
-  
+
   return {
     first_name: firstName,
     last_name: lastName,
@@ -35,7 +36,7 @@ function extractCustomerData(body) {
         country: "US"
       },
     ],
-    "accepts_marketing": true, 
+    "accepts_marketing": true,
   };
 }
 
@@ -56,67 +57,15 @@ async function createOrUpdateCustomer(data) {
   const responseData = await response.json();
 
   if (!response.ok) {
-    return handleCustomerErrors(responseData, data.email, data.phone);
+    throw new Error('Error creating customer.');
   }
 
   return responseData.customer;
 }
 
-async function handleCustomerErrors(responseData, email, phone) {
-  if (responseData.errors) {
-    if (responseData.errors.email && responseData.errors.email.includes("has already been taken")) {
-      return findExistingCustomerByEmail(email);
-    }
-
-    if (responseData.errors.phone && responseData.errors.phone.includes("Phone has already been taken")) {
-      return findExistingCustomerByPhone(phone);
-    }
-  }
-
-  throw new Error('Error creating customer.');
-}
-
-async function findExistingCustomerByEmail(email) {
-  const baseUrl = process.env.SHOPIFY_URL;
-  const searchResponse = await fetch(`${baseUrl}/customers/search.json?query=email:${email}`, {
-    method: 'GET',
-    headers: {
-      'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const searchResult = await searchResponse.json();
-
-  if (searchResult.customers.length > 0) {
-    return searchResult.customers[0];
-  }
-
-  throw new Error('Server error. Unable to locate existing customer.');
-}
-
-async function findExistingCustomerByPhone(phone) {
-  const baseUrl = process.env.SHOPIFY_URL;
-  const searchResponse = await fetch(`${baseUrl}/customers/search.json?query=phone:${phone}`, {
-    method: 'GET',
-    headers: {
-      'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const searchResult = await searchResponse.json();
-
-  if (searchResult.customers.length > 0) {
-    return searchResult.customers[0];
-  }
-
-  throw new Error('Server error. Unable to locate existing customer.');
-}
-
 async function checkForRecentReplacementOrders(customer, customerData) {
   const baseUrl = process.env.SHOPIFY_URL;
-  const oneDayAgo = new Date(Date.now() - 24*60*60*1000).toISOString();
+  const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString();
   const ordersUrl = `${baseUrl}/orders.json?status=any&created_at_min=${oneDayAgo}&tag=Instant Replacement`;
 
   const options = {
@@ -138,15 +87,13 @@ async function checkForRecentReplacementOrders(customer, customerData) {
   });
 
   const matchingAddressOrders = responseData.orders.filter(order => {
-    const hasMatchingAddress = 
+    const hasMatchingAddress =
       order.shipping_address.address1 === customerAddress.address1 &&
       order.shipping_address.city === customerAddress.city &&
       order.shipping_address.zip === customerAddress.zip;
-   
+
     return hasMatchingAddress;
   });
-
-
 
   if (matchingCustomerOrders.length > 0 || matchingAddressOrders.length > 0) {
     throw new Error('A replacement order has already been placed with this address or by this customer in the last 24 hours.');
@@ -179,7 +126,7 @@ function extractOrderData(body, customer) {
           price: "0.00"
         }
       ],
-      "tags": 'Instant Replacement' 
+      "tags": 'Instant Replacement'
     }
   };
 }
@@ -205,4 +152,17 @@ async function createOrder(orderData) {
   }
 
   return orderResponseData;
+}
+
+async function notifyErrorToSlack(message) {
+  const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+  const body = {
+    text: `Server error: ${message}`,
+  };
+
+  await fetch(SLACK_WEBHOOK_URL, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
