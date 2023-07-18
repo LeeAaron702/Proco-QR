2d52811068035bc0e70d82e72c3f1c2b997e84c1
Lee Seaver <lee.seaver@gmail.com>
2023-07-18 00:10:32 -0700

    autocomplete address 6

--- api/autocomplete.js
+++ api/autocomplete.js
@@ -1,15 +1,16 @@
 const autocomplete = async (req, res) => {
-  const { input } = req.body;
-  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("🚀 ~ file: autocomplete.js:8 ~ autocomplete ~ data:", data)
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching autocomplete options' });
-  }
-};
-
-export default autocomplete;
+    const { input } = req.body;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&components=country:US`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("🚀 ~ file: autocomplete.js:8 ~ autocomplete ~ data:", data)
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching autocomplete options' });
+    }
+  };
+  
+  export default autocomplete;
+  
